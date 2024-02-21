5ea0e3cf5d12f4bc629f22cdf848667c4e0a01f3
choinseong <choinseong2237@gmail.com>
2024-02-21 14:13:15 +0900

    main페이지 꾸미기

--- pages/main.php
+++ pages/main.php
@@ -8,11 +8,30 @@ if (isset($_SESSION['user_idx'])) {
 
 
 ?>
-<h1>메인이다 병신아ㅋ</h1>
-<a href="register">회원가입</a>
-<a href="login">로그인</a>
-<a href="postpage">게시글</a>
-<form method="post">
-    <button id="logout" name="logout" type="submit"><a href="logout">로그아웃</a></button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            width: 100%;
+            height: 100%;
+            background: url("./toothless.gif");
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+    </style>
+</head>
+<body>
+    <h1>메인이다 병신아ㅋ</h1>
+    <a href="register">회원가입</a>
+    <a href="login">로그인</a>
+    <a href="postpage">게시글</a>
+    <form method="post">
+        <button id="logout" name="logout" type="submit"><a href="logout">로그아웃</a></button>
+    </form>
+</body>
+</html>
 
