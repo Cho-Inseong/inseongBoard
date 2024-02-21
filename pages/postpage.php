438d06af048521ac65e55bd45d7f82531444891e
choinseong <choinseong2237@gmail.com>
2024-02-21 16:38:16 +0900

    게시글 DB에서 불러오기

--- pages/postpage.php
+++ pages/postpage.php
@@ -1,11 +1,28 @@
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <button><a href="newpost">글쓰기</a></button>
-</body>
-</html>
+<?php
+$sql = "SELECT * FROM posts WHERE is_deleted = 0 ORDER BY write_date DESC ";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<button><a href="newpost">글쓰기</a></button>
+
+<div class="container mt-5">
+    <h1>게시글 목록</h1>
+    <?php
+        if ($posts) {
+            foreach ($posts as $post) {
+                $divinnertext = "";
+                $divinnertext .= "<div class = 'post'>";
+                $divinnertext .= "<h2>" . $post["title"]. "</h2>";
+                $divinnertext .= "<p> 작성자 ID: " . $post["user_idx"] . "</p>";
+                $divinnertext .= "<p> 게시글 ID: " . $post["post_idx"] . "</p>";
+                $divinnertext .= "<p>" . $post["content"] . "</p>";
+                $divinnertext .= "</div>";
+                echo $divinnertext;
+            }
+        }    
+    ?>
+</div>
+
