2fda4b5f49d4d4a9dc4c1093abef69d267fffe37
choinseong <choinseong2237@gmail.com>
2024-02-22 16:22:45 +0900

    관리자 페이지 접속 시 삭제 버튼과 함께 게시글이 출력되고, 삭제 버튼을 누르면 관리자의 컨펌 후 게시글 삭제

--- index.php
+++ index.php
@@ -53,5 +53,5 @@ echo "<script>console.log('path[1] = " . $path[1] . "');</script>";
     ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script> 
 </html>
--- pages/admin.php
+++ pages/admin.php
@@ -1,5 +1,5 @@
 <?php
-if(!isset($_SESSION["user_idx"])) {
+if (!isset($_SESSION["user_idx"])) {
     echo "
     <script>
     alert('로그인 후 이용 가능합니다.')
@@ -7,7 +7,7 @@ if(!isset($_SESSION["user_idx"])) {
     </script>
     ";
 } else {
-    if(($_SESSION["is_admin"] == 0)) {
+    if (($_SESSION["is_admin"] == 0)) {
         echo "
         <script>
         alert('관리자만 사용가능합니다.')
@@ -17,20 +17,27 @@ if(!isset($_SESSION["user_idx"])) {
     }
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $sql = "UPDATE `posts` SET `is_deleted` = '1' WHERE post_idx = :post_idx";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':post_idx', $_POST["post_idx"]);
+    $stmt->execute();
+}
 
 $sql = "SELECT * FROM posts WHERE is_deleted = 0 ORDER BY write_date DESC ";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $delete = $post["post_idx"];
-    $updatesql = "UPDATE `posts` SET `is_deleted` = '1' WHERE post_idx = :post_idx";
-    $upstmt = $pdo->prepare($updatesql);
-    $upstmt->bindParam(":post_idx", $delete);
-    $upstmt->execute();
-    $upuser = $upstmt->fetch(PDO::FETCH_ASSOC);
-}
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $delete = $post["post_idx"];
+//     $updatesql = "UPDATE `posts` SET `is_deleted` = '1' WHERE post_idx = :post_idx";
+//     $upstmt = $pdo->prepare($updatesql);
+//     $upstmt->bindParam(":post_idx", $delete);
+//     $upstmt->execute();
+//     $upuser = $upstmt->fetch(PDO::FETCH_ASSOC);
+// }
+
 
 ?>
 <h1>관리자 페이지 입니다.</h1>
@@ -38,24 +45,54 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="container mt-5">
     <h1>게시글 목록</h1>
     <?php
-        if ($posts) {
-            foreach ($posts as $post) {
-                $sql = "SELECT * FROM users WHERE user_idx = :user_idx";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(":user_idx", $post["user_idx"]);
-                $stmt->execute();
-                $user = $stmt->fetch(PDO::FETCH_ASSOC);
-                $divinnertext = "";
-                $divinnertext .= "<div class = 'post'>";
-                $divinnertext .= "<h2>" . $post["title"]. "</h2>";
-                $divinnertext .= "<p> 작성자 : " . $user["username"] . "</p>";
-                $divinnertext .= "<p> 게시글 ID: " . $post["post_idx"] . "</p>";
-                $divinnertext .= "<p> 작성일: " . $post["write_date"] . "</p>";
-                $divinnertext .= "<p>" . $post["content"] . "</p>";
-                $divinnertext .= "</div>";
-                $divinnertext .= "<button type='submit' name='delete'>삭제버튼</button>";
-                echo $divinnertext;
-            }
-        }    
+    if ($posts) {
+        foreach ($posts as $post) {
+            $sql = "SELECT * FROM users WHERE user_idx = :user_idx";
+            $stmt = $pdo->prepare($sql);
+            $stmt->bindParam(":user_idx", $post["user_idx"]);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            $divinnertext = "";
+            $divinnertext .= "<div class = 'post'>";
+            $divinnertext .= "<h2>" . $post["title"] . "</h2>";
+            $divinnertext .= "<p> 작성자 : " . $user["username"] . "</p>";
+            $divinnertext .= "<p> 게시글 ID: " . $post["post_idx"] . "</p>";
+            $divinnertext .= "<p> 작성일: " . $post["write_date"] . "</p>";
+            $divinnertext .= "<p>" . $post["content"] . "</p>";
+            $divinnertext .= "<button id='" . $post["post_idx"] . "' onclick='postdelete(this);'>삭제버튼</button>";
+            $divinnertext .= "</div>";
+
+            echo $divinnertext;
+        }
+    }
     ?>
 </div>
+
+
+
+<script>
+    function postdelete(elem) {
+        const post_idx = elem.id;
+        const isConfirm =  confirm(`정말로 ${post_idx}번 게시글을 삭제하시겠습니까?`);
+        if (isConfirm) {
+            $.ajax({
+                url:'admin',
+                type: 'POST',
+                data: {"post_idx": post_idx},
+                success: function() {
+                    alert("정삭적으로 삭제했다잉");
+                    location.href ='admin';
+                },
+                error: function() {
+                    alert("에큥 실패★")
+                }
+            })
+            // alert("지웠다 이 시발롬아 이제 속 시원하니?")
+            // console.log("지울게용");
+        } else {
+            alert("시발련아 왜 안지움?")
+            console.log("병신 쪼다 새끼 왜 지우다 마냐ㅋㅋ")
+        }
+        return console.log(elem)
+    }
+</script>
