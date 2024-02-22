a628e5b95c3d74be949193eb49b964350496d648
choinseong <choinseong2237@gmail.com>
2024-02-22 15:29:29 +0900

    게시물 업데이트 (중간 완정)(후에 내가 해볼 거)

--- pages/admin.php
+++ pages/admin.php
@@ -16,5 +16,46 @@ if(!isset($_SESSION["user_idx"])) {
         ";
     }
 }
+
+
+$sql = "SELECT * FROM posts WHERE is_deleted = 0 ORDER BY write_date DESC ";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $delete = $post["post_idx"];
+    $updatesql = "UPDATE `posts` SET `is_deleted` = '1' WHERE post_idx = :post_idx";
+    $upstmt = $pdo->prepare($updatesql);
+    $upstmt->bindParam(":post_idx", $delete);
+    $upstmt->execute();
+    $upuser = $upstmt->fetch(PDO::FETCH_ASSOC);
+}
+
 ?>
 <h1>관리자 페이지 입니다.</h1>
+
+<div class="container mt-5">
+    <h1>게시글 목록</h1>
+    <?php
+        if ($posts) {
+            foreach ($posts as $post) {
+                $sql = "SELECT * FROM users WHERE user_idx = :user_idx";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(":user_idx", $post["user_idx"]);
+                $stmt->execute();
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                $divinnertext = "";
+                $divinnertext .= "<div class = 'post'>";
+                $divinnertext .= "<h2>" . $post["title"]. "</h2>";
+                $divinnertext .= "<p> 작성자 : " . $user["username"] . "</p>";
+                $divinnertext .= "<p> 게시글 ID: " . $post["post_idx"] . "</p>";
+                $divinnertext .= "<p> 작성일: " . $post["write_date"] . "</p>";
+                $divinnertext .= "<p>" . $post["content"] . "</p>";
+                $divinnertext .= "</div>";
+                $divinnertext .= "<button type='submit' name='delete'>삭제버튼</button>";
+                echo $divinnertext;
+            }
+        }    
+    ?>
+</div>
